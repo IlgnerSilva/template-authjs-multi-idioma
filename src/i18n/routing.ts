da838035405f15067d88cb1358fe4a74ad1e8da6
mahoraga <mahoraga@localhost.localdomain>
2024-11-15 12:32:36 -0300

    add middleware para verificar se pode ou não acessar a rota

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -7,7 +7,21 @@ export const routing = defineRouting({
 
 	// Idioma padrão
 	defaultLocale: 'en-US',
+
+	// Caminhos para cada idioma
+	pathnames: {
+		'/': '/',
+		'/auth/login': {
+			'en-US': '/auth/login',
+			'pt-BR': '/auth/login',
+		},
+	},
 })
+
+export type Pathnames = keyof typeof routing.pathnames
+export type Locale = (typeof routing.locales)[number]
+
+export const { locales, defaultLocale, pathnames } = routing
 
 // Cria as funções de navegação
 // Link, redirect, usePathname, useRouter
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,39 +1,77 @@
+import { authRoutes, publicRoutes } from '@/config/routes'
 import NextAuth from 'next-auth'
 import createMiddleware from 'next-intl/middleware'
+import { type NextRequest, NextResponse } from 'next/server'
 import authConfig from './auth.config'
-import { routing } from './i18n/routing'
+import { locales, routing } from './i18n/routing'
 
-// Configurações do NextAuth
+// Configuração do NextAuth com base no arquivo de configuração auth.config
 const { auth } = NextAuth(authConfig)
 
-// Middleware de autenticação
-const authMiddleware = (req) => {
+// Middleware responsável pela internacionalização das rotas
+const intlMiddleware = createMiddleware(routing)
+
+// Função que testa se o caminho da URL corresponde a uma das rotas definidas
+const testPathnameRegex = (pages: string[], pathName: string): boolean => {
+	// Substitui rotas dinâmicas (com parâmetros) por expressões regulares
+	const pathsWithParams = pages.map((p) => p.replace(/\[.*?\]/g, '[^/]+'))
+
+	// Cria uma expressão regular para testar se o caminho (pathName) corresponde a uma das rotas
+	return RegExp(
+		`^(/(${locales.join('|')}))?(${pathsWithParams.flatMap((p) => (p === '/' ? ['', '/'] : p)).join('|')})/?$`,
+		'i', // flag "i" para ignorar maiúsculas/minúsculas
+	).test(pathName)
+}
+
+// Middleware de autenticação usando NextAuth
+const authMiddleware = auth((req) => {
+	// Verifica se a página solicitada faz parte das rotas de autenticação
+	const isAuthPage = testPathnameRegex(authRoutes, req.nextUrl.pathname)
+	// Verifica se o usuário está autenticado
 	const isLogged = !!req.auth
-	console.log(req.nextUrl.locale)
-	if (!isLogged) {
-		// Redirecionar ou fazer algo se o usuário não estiver autenticado
+
+	// Se o usuário não estiver autenticado e tentar acessar uma página que requer autenticação, redireciona para a página de login
+	if (!isLogged && isAuthPage) {
+		return NextResponse.redirect(new URL('/auth/login', req.nextUrl))
+	}
+
+	// Se o usuário estiver autenticado e tentar acessar uma página de login, redireciona para a página inicial
+	if (isLogged && isAuthPage) {
+		return NextResponse.redirect(new URL('/', req.nextUrl))
+	}
+
+	// Se não for uma página de autenticação, continua o processo de internacionalização
+	return intlMiddleware(req)
+})
+
+// Função principal do middleware que combina a autenticação e a internacionalização
+const middleware = (req: NextRequest) => {
+	// Verifica se a página solicitada é uma página pública (sem necessidade de autenticação)
+	const isPublicPage = testPathnameRegex(publicRoutes, req.nextUrl.pathname)
+	// Verifica se a página solicitada é uma página de autenticação
+	const isAuthPage = testPathnameRegex(authRoutes, req.nextUrl.pathname)
+
+	// Se for uma página de autenticação, executa o middleware de autenticação
+	if (isAuthPage) {
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		return (authMiddleware as any)(req)
+	}
+
+	// Se for uma página pública, executa o middleware de internacionalização
+	if (isPublicPage) {
+		return intlMiddleware(req)
+		// biome-ignore lint/style/noUselessElse: <explanation>
+	} else {
+		// Caso contrário, executa o middleware de autenticação (para páginas protegidas)
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		return (authMiddleware as any)(req)
 	}
 }
 
-// Middleware de internacionalização
-const intlMiddleware = createMiddleware(routing)
-
-// Função para combinar os middlewares
-export default async function middleware(req) {
-	// Primeiro, execute a lógica do NextAuth (autenticação)
-	await authMiddleware(req)
-
-	// Em seguida, execute a lógica do Next-Intl (internacionalização)
-	return intlMiddleware(req)
-}
-
-// Configuração de correspondência para os caminhos
+// Configuração de correspondência para os caminhos que o middleware deve interceptar
 export const config = {
-	matcher: [
-		// Exclui as rotas de API e arquivos estáticos
-		'/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-		// Adiciona as rotas de internacionalização
-		'/',
-		'/(pt-BR|en-US)/:path*',
-	],
+	// A expressão regular garante que o middleware será aplicado a todas as rotas, exceto aquelas para API, Next.js e arquivos estáticos
+	matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
 }
+
+export default middleware
