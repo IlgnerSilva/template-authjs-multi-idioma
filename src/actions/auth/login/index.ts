637db216b90e2f0c2cbaa741828d6686afb5d607
IlgnerSilva <ilgnersilva@outlook.com>
2024-11-21 01:10:40 -0300

    feat: traduzindo para inglês

--- src/actions/auth/login/index.ts
+++ src/actions/auth/login/index.ts
@@ -3,6 +3,7 @@
 import { signIn } from '@/auth'
 import { CredentialsSchema } from '@/schemas/auth'
 import { findByUserEmail } from '@/services/user'
+import { getTranslations } from 'next-intl/server'
 import type { z } from 'zod'
 
 export const loginGoogle = async () => await signIn('google')
@@ -11,8 +12,10 @@ export async function loginCredentials(
 	credentials: z.infer<typeof CredentialsSchema>,
 ) {
 	const validCredentials = await CredentialsSchema.safeParse(credentials)
+	const p = await getTranslations('Pages')
 
-	if (!validCredentials.success) return { error: true, message: 'Invalid data' }
+	if (!validCredentials.success)
+		return { error: true, message: p('Login.messages.error.credentials') }
 
 	try {
 		const { email, password, code } = validCredentials.data
