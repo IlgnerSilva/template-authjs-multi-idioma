e364849633404056e2933be3edf00d5ca91ed921
IlgnerSilva <ilgnersilva@outlook.com>
2024-11-22 00:51:21 -0300

    feat: add component SwitcherLocale para poder alternar entre idiomas

--- src/app/[locale]/auth/login/page.tsx
+++ src/app/[locale]/auth/login/page.tsx
@@ -1,4 +1,5 @@
 import { LoginForm } from '@/components/auth/login-form'
+import { SwitcherLocale } from '@/components/utils/switcher-locale'
 
 export default function Page() {
 	return (
---
+++ src/components/utils/switcher-locale.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select'
+import { locales, usePathname } from '@/i18n/routing'
+import { usePathname as usePath, useRouter } from 'next/navigation'
+
+export function SwitcherLocale() {
+	const pathname = usePathname() // Defina dentro do componente de função
+	const router = useRouter()
+	const getLocale = usePath().split('/')[1].toUpperCase()
+
+	const handleLocaleChange = (locale: string) => {
+		// Mude o caminho para incluir o novo locale
+		const href = `/${locale}${pathname}`
+		router.push(href)
+	}
+
+	return (
+		<Select onValueChange={(e) => handleLocaleChange(e)}>
+			<SelectTrigger className="w-24 p-1 max-h-max">
+				<SelectValue placeholder={getLocale} />
+			</SelectTrigger>
+			<SelectContent>
+				{locales.map((locale) => (
+					<SelectItem key={locale} value={locale} className="cursor-pointer">
+						{locale.toUpperCase()}
+					</SelectItem>
+				))}
+			</SelectContent>
+		</Select>
+	)
+}
