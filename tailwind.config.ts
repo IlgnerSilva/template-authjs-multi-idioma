1e2b0ae31fe350aa903e465243464c892f37622a
IlgnerSilva <ilgnersilva@outlook.com>
2025-01-28 07:31:15 -0300

    feat: implementando seidebar do shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,52 +8,62 @@ const config: Config = {
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
-		extend: {
-			colors: {
-				background: 'var(--background)',
-				foreground: 'var(--foreground)',
-				neutral: {
-					base: {
-						100: '#FFFFFF', // Mais claro
-						200: '#EFEFEF',
-						300: '#F4F4F4',
-						400: '#EFEFEF',
-						500: '#6F767E', // Neutro médio
-						600: '#33383F',
-						700: '#272B30',
-						800: '#1A1D1F',
-						900: '#111315', // Mais escuro
-					},
-				},
-				shades: {
-					base: {
-						200: '#9A9FA5',
-						400: '#6F767E',
-						600: '#6F767E',
-						800: '#33383F',
-					},
-				},
-				primary: {
-					blue: '#2A85FF', // Azul principal
-					green: '#83BF6E', // Verde principal
-					red: '#FF6A55', // Vermelho principal
-					purple: '#8E59FF', // Roxo principal
-				},
-				secondary: {
-					orange: '#FFBC99', // Secundário laranja
-					lavender: '#CABDFF',
-					lightblue: '#B1E5FC',
-					mint: '#B5E4CA',
-					yellow: '#FFD88D',
-				},
-			},
-			borderRadius: {
-				lg: 'var(--radius)',
-				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)',
-			},
-		},
-	},
+    	extend: {
+    		colors: {
+    			background: 'var(--background)',
+    			foreground: 'var(--foreground)',
+    			neutral: {
+    				base: {
+    					'100': '#FFFFFF',
+    					'200': '#EFEFEF',
+    					'300': '#F4F4F4',
+    					'400': '#EFEFEF',
+    					'500': '#6F767E',
+    					'600': '#33383F',
+    					'700': '#272B30',
+    					'800': '#1A1D1F',
+    					'900': '#111315'
+    				}
+    			},
+    			shades: {
+    				base: {
+    					'200': '#9A9FA5',
+    					'400': '#6F767E',
+    					'600': '#6F767E',
+    					'800': '#33383F'
+    				}
+    			},
+    			primary: {
+    				blue: '#2A85FF',
+    				green: '#83BF6E',
+    				red: '#FF6A55',
+    				purple: '#8E59FF'
+    			},
+    			secondary: {
+    				orange: '#FFBC99',
+    				lavender: '#CABDFF',
+    				lightblue: '#B1E5FC',
+    				mint: '#B5E4CA',
+    				yellow: '#FFD88D'
+    			},
+    			sidebar: {
+    				DEFAULT: 'hsl(var(--sidebar-background))',
+    				foreground: 'hsl(var(--sidebar-foreground))',
+    				primary: 'hsl(var(--sidebar-primary))',
+    				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+    				accent: 'hsl(var(--sidebar-accent))',
+    				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+    				border: 'hsl(var(--sidebar-border))',
+    				ring: 'hsl(var(--sidebar-ring))'
+    			}
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	}
+    },
 	plugins: [require('tailwindcss-animate')],
 };
 export default config;
